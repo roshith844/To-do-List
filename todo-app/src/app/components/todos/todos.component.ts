5a4e61741b57e4c1dde8be603d0f611e25b3313c
Roshith P <roshith844@gmail.com>
2023-01-11 11:10:45 +0530

    chore: reformat

--- todo-app/src/app/components/todos/todos.component.ts
+++ todo-app/src/app/components/todos/todos.component.ts
@@ -3,41 +3,39 @@ import { Todo } from './../../model/Todo';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent {
   todos!: Todo[];
-  inputTodo:string = ""
-constructor(){
-
-}
-  ngOnInit(){
-    this.todos = [{
-      content: 'first todo',
-      completed: false
-    },
-    {
-      content: 'second todo',
-      completed: true
-    }]
-
+  inputTodo: string = '';
+  constructor() {}
+  ngOnInit() {
+    this.todos = [
+      {
+        content: 'first todo',
+        completed: false,
+      },
+      {
+        content: 'second todo',
+        completed: true,
+      },
+    ];
   }
-  addTodo(){
+  addTodo() {
     this.todos.push({
       content: this.inputTodo,
-      completed: false
-    })
-this.inputTodo =""
+      completed: false,
+    });
+    this.inputTodo = '';
   }
-  toggleDone(id:number): void{
-    this.todos.map((v,i)=>{
-      if(i == id)  v.completed = !v.completed
-    })
+  toggleDone(id: number): void {
+    this.todos.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+    });
   }
-  deleteTodo(id: number){
-    this.todos = this.todos.filter((value, index)=>{
-      return index != id
-    })
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter((value, index) => {
+      return index != id;
+    });
   }
-
 }
